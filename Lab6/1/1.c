4cf09a8a7b7701f11dcf4af99b54544847ed91b0
Shpitko Egor <egor.shpitko.00@mail.ru>
2019-05-31 12:10:18 +0300

    L6_1

--- Lab6/1/1.c
+++ Lab6/1/1.c
@@ -111,12 +111,25 @@ HandleInput(struct dpoint* mas, int n, int flg)
 	return (flg)?n:size;
 }
 
-void
-genDigParam(struct dpoint* mas, int n)
+double
+F0(double a)
 {
+	a /= sqrt(2);
+	double res = erf(a/sqrt(2))/2;
+	return res;
+}
+
+void
+CheckNorm(struct dpoint* mas, int n, double a, int tmp2)
+{
+	double p[n];
+	double np[n];
+	double row[n];
 	double x = 0.0;
 	double D = 0.0;
 	double sig = 0.0;
+	double S2 = 0.0;
+	double s = 0.0;
 	int sum = 0;
 	for(int i = 0; i < n; i++){
 		x += mas[i].cent * mas[i].n;
@@ -127,6 +140,30 @@ genDigParam(struct dpoint* mas, int n)
 	D /= sum;
 	D -= x*x;
 	sig = sqrt(D);
+	S2 = D*sum/(sum-1);
+	s = sqrt(S2);
+	printf("x is %.3lf\n",x);
+	printf("s is %.3lf\n",s);
+	if(tmp2){
+		p[0] = F0((mas[0].b-x)/sig) + 0.5;
+		for(int i = 1; i < n-1; i++){
+			p[i] = F0((mas[i].b-x)/sig) - F0((mas[i].a-x)/sig);
+		}
+		p[n-1] = 0.5 - F0((mas[n-1].a-x)/sig);
+	}else{
+		p[0] = F0((mas[1].cent - x)/sig) + 0.5;
+		for(int i = 1; i < n-1; i++){
+			p[i] = F0((mas[i+1].cent - x)/sig) - F0((mas[i].cent - x)/sig);
+		}
+		p[n-1] = 0.5 - F0((mas[n-1].cent - x)/sig);
+	}
+	double X2 = 0;
+	for(int i = 0; i < n; i++){
+		np[i] = p[i]*sum;
+		row[i] = (mas[i].n-np[i])*(mas[i].n-np[i])/np[i];
+		X2 += row[i];
+		printf("X2 is: %.5lf\n",p[i]);
+	}
 }
 
 int
@@ -135,6 +172,7 @@ main(void)
 	struct dpoint mas[100];
 	printf("0 - ввод из файла\n");
 	printf("1 - ручной ввод\n");
+	int tmp;
 	int tmp1;
 	int tmp2;
 	int n = 0;
@@ -153,9 +191,19 @@ main(void)
 	}else{
 		n = generateMas(mas,"data.txt",tmp2);
 	}
-	genDigParam(mas,n);
-
 	printf("N is %d\n",n);
+	printf("Проверка гипотезы о виде распределения по критерию Пирсона\n");
+	printf("0 - о нормальном распределении\n");
+	printf("1 - о биномиальном распределении\n");
+	scanf("%d",&tmp);
+	printf("Введите a:\n");
+	double a;
+	scanf("%lf",&a);
+	if (tmp){
+		// CheckBin(mas,n,a);
+	}else{
+		CheckNorm(mas,n,a,tmp2);
+	}
 	printf("Введите целое число для завершения\n");
 	scanf("%d",&n);
 	// delData(tmp);
